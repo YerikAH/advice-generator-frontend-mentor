67107c278a12003899402519f27d23c92ad5af41
Harvey <yerik05vh@gmail.com>
2022-10-24 16:17:42 -0500

    async await js

---
+++ js/src/asyncAwait.js
@@ -0,0 +1,53 @@
+// HTML Element
+
+const d = document;
+let $numberAdvice = d.getElementById("numberAdvice");
+let $messageAdvice = d.getElementById("messageAdvice");
+let $otherAdviceButton = d.getElementById("otherAdviceButton");
+let $adviceInfo = d.getElementById("adviceInfo");
+let $loader = d.getElementById("loader");
+
+// Async await
+async function getAdvice(numberRandomSearch) {
+  let url = `https://api.adviceslip.com/advice/${numberRandomSearch}`;
+
+  switchLoader("active-get");
+
+  try {
+    let rst = await fetch(url);
+    let json = await rst.json();
+
+    if (!rst.ok) {
+      throw {
+        statusText: rst.statusText,
+        status: rst.status,
+      };
+    }
+
+    let numberAdvice = `${(await json).slip.id}`;
+    let messageAdvice = `${(await json).slip.advice}`;
+
+    $numberAdvice.textContent = `#${numberAdvice}`;
+    $messageAdvice.textContent = `“${messageAdvice}”`;
+    switchLoader("active-get");
+  } catch (err) {
+    switchLoader("active-get");
+    $numberAdvice.textContent = `#404`;
+    $messageAdvice.textContent = `“Sorry, an error occurred, please try again.”`;
+  }
+}
+
+$otherAdviceButton.addEventListener("click", getAgain);
+
+function switchLoader(active) {
+  $loader.classList.toggle(active);
+  $adviceInfo.classList.toggle(active);
+}
+
+function getAgain() {
+  let randomNumber = Math.round(Math.random() * 220);
+  getAdvice(randomNumber);
+}
+// open app
+let numberTemp = 117;
+getAdvice(numberTemp);
